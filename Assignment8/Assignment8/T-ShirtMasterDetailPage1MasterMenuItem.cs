1dda5d70fa5be7ecc1a46c97aeefdeadc83d003f
Mabuza98 <70321095+Mabuza98@users.noreply.github.com>
2020-12-03 15:12:11 +0200

    Add project files.

---
+++ Assignment8/Assignment8/T-ShirtMasterDetailPage1MasterMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8
+{
+    public class T_ShirtMasterDetailPage1MasterMenuItem
+    {
+        public T_ShirtMasterDetailPage1MasterMenuItem()
+        {
+            TargetType = typeof(T_ShirtMasterDetailPage1MasterMenuItem);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
---
+++ Assignment8/Assignment8/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Assignment8.ViewModels
+{
+
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private string _demoText;
+
+        public string DemoText
+        {
+            get { return _demoText; }
+            set { _demoText = value;
+                OnPropertyChanged("DemoText");
+            }
+        }
+
+        public ICommand TestButtonCommand { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            var propertyHandler = PropertyChanged;
+
+            if (propertyHandler != null)
+            {
+                propertyHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+
+        }
+       
+        private void TestButton()
+        {
+            DemoText = "Trigger the shooter";
+        }
+    }
+}
